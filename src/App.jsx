d2ce9a984ed6ba18c8bd38a254c31c4221ecb557
Jaya <jayavashist050svj@gmail.com>
2024-05-22 18:09:29 +0530

    Funtionality Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,71 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { v4 as uuidv4 } from "uuid";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 function App() {
-  const [task, setTask] = useState("your task");
-  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState("");
+  const [showFinished, setshowFinished] = useState(true);
 
+  const [tasks, setTasks] = useState(() => {
+    let tasksString = localStorage.getItem("todos");
+    return tasksString ? JSON.parse(tasksString) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const toggleFinished = () => {
+    setshowFinished(!showFinished);
+  };
+
+  //handle the input field
   const handleChange = (e) => {
     setTask(e.target.value);
   };
 
-  const handleSave = () => {};
+  //handle save button
+  const handleSave = () => {
+    if (task.trim().length >= 1) {
+      setTasks([...tasks, { id: uuidv4(), task, isCompleted: false }]);
+      setTask("");
+    }
+  };
 
-  const handleEdit = () => {};
+  const handleEdit = (e, id) => {
+    let ediTask = tasks.filter((item) => {
+      return item.id === id;
+    });
+    setTask(ediTask[0].task);
 
-  const handleDelete = () => {};
+    let newTasks = tasks.filter((item) => {
+      return item.id != id;
+    });
+    setTasks(newTasks);
+  };
+
+  const handleDelete = (e, id) => {
+    let newTasks = tasks.filter((item) => {
+      return item.id != id;
+    });
+    setTasks(newTasks);
+  };
+
+  const handleCheckBox = (e) => {
+    let id = e.target.name;
+    console.log(id);
+    //find to do with given id
+    let index = tasks.findIndex((item) => {
+      return item.id === id;
+    });
+
+    let newTasks = [...tasks];
+    newTasks[index].isCompleted = !newTasks[index].isCompleted;
+    setTasks(newTasks);
+  };
 
   return (
     <>
@@ -22,35 +73,80 @@ function App() {
 
       <div className="card container">
         <div className="header">
-          <h2>iTask- Manage your todos at one place</h2>
+          <h2>iTask- Manage your Tasks at one place</h2>
         </div>
-        <div>Add your ToDo</div>
         <div className="create">
           <input name="todo" value={task} type="text" onChange={handleChange} />
           <button className="savebtn" onClick={handleSave}>
             Save
           </button>
         </div>
+        <div className="showfinished">
+          {" "}
+          <input
+            onChange={toggleFinished}
+            type="checkbox"
+            checked={showFinished}
+            name="showfinished"
+          />
+          Show finished
+        </div>
+
         <hr />
-        <div>
-          <h4>Your Todos</h4>
-        </div>
-        <div className="todos">
-          <div className="text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
-            commodi?
+        {tasks.length === 0 ? (
+          <div className="no-task"> Yay! No Task </div>
+        ) : (
+          <div>
+            <div>
+              <h3>Your Tasks</h3>
+            </div>
+
+            <div className="tasks">
+              {tasks.map((item) => {
+                if (!showFinished && item.isCompleted) return;
+                return (
+                  <div className="task" key={item.id}>
+                    <div className="task-item">
+                      <input
+                        type="checkbox"
+                        name={item.id}
+                        checked={item.isCompleted}
+                        className="is-complete"
+                        onChange={handleCheckBox}
+                      />
+                      <div
+                        className={
+                          !item.isCompleted ? "text" : "line-through text"
+                        }
+                      >
+                        <p>{item.task}</p>
+                      </div>
+                    </div>
+                    <div className="buttons">
+                      <button
+                        className="edit"
+                        name={item.id}
+                        onClick={(e) => {
+                          handleEdit(e, item.id);
+                        }}
+                      >
+                        <FaEdit />
+                      </button>
+                      <button
+                        className="delete"
+                        onClick={(e) => {
+                          handleDelete(e, item.id);
+                        }}
+                      >
+                        <MdDelete />
+                      </button>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className="buttons">
-            <button className="edit" onClick={handleEdit}>
-              Edit
-            </button>
-            <button className="delete" onClick={handleDelete}>
-              delete
-            </button>
-          </div>
-        </div>
-        <div className="todos"></div>
-        <div className="todos"></div>
+        )}
       </div>
     </>
   );
